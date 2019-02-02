cae90c43e158d8ae99a0e7d2efa2f7d6f26ac2a7
alexandergureev <g.alex00@bk.ru>
2019-02-02 03:05:36 +0300

    Added notification if an attempt is made to confirm mail without authorization

--- src/components/Common/PrivateRoute.js
+++ src/components/Common/PrivateRoute.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { useStore } from "easy-peasy";
 import { Route, Redirect } from "react-router-dom";
+import MessageConstructor from "./LoadingMessage";
+
+const message = MessageConstructor();
+
+const isConfirmEmailRoute = ({ pathname = "", search = "" }, isAuth) => {
+  const validPathname = "/managed/verify";
+  if (search && pathname === validPathname && !isAuth) {
+    setTimeout(
+      () => message.error("You must be logged in to verify your mail."),
+      1500
+    );
+  }
+};
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const { isAuth } = useStore(state => state.session);
-  const { location } = rest;
+  isConfirmEmailRoute(rest.location, isAuth);
+
   return (
     <Route
       {...rest}
@@ -15,7 +29,10 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
           <Redirect
             to={{
               pathname: "/login",
-              state: { pathname: location.pathname, search: location.search }
+              state: {
+                pathname: props.location.pathname,
+                search: props.location.search
+              }
             }}
           />
         )
